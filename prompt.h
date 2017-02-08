18b13b6cf2db557f579f22c4d1940b5d95220005
gregjohnson2017 <gregjohnson2017@gmail.com>
2017-02-08 09:08:44 -0600

    Initial commit of essential source files

---
+++ prompt.h
@@ -0,0 +1,13 @@
+#ifndef _PROMPT_H
+#define _PROMPT_H
+#include "gregutils.h"
+#include "stdbool.h"
+extern int round;
+const extern int character_type, max_rounds;
+char* get_player(char*);
+char* get_type(char*);
+char* get_prompt(char*);
+char** get_options(char*);
+char** get_targets(char*);
+bool do_target(char**, char*);
+#endif
