193c69d47bb6ada83df172fef55b94bbd0d944bd
rainstorm124 <rainiercarlos.b.bravo@gmail.com>
2017-03-15 10:16:40 -0500

    Updated Zoinkas

--- zoinka.h
+++ zoinka.h
@@ -5,6 +5,12 @@
 #include "prompt.h"
 extern int round;
 const extern int character_type, max_rounds;
-char* add_update_to_queue(char* option_code);
+bool add_update_to_queue(char* option_code);
 int count_players_chosen(FILE *fp);
+bool admin_override();
+bool stak_vote_spcial(int stak_votes, char **voter_codes, char **voter_names);
+void YEZ_favor_special();
+void YAG_favor_special();
+void YEZ_cred_special();
+void YAG_cred_special();
 #endif
